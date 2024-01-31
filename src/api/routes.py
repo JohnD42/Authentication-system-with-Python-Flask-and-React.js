ba37cbc716f59797e6ea15201b2901a0c14dd3f3
John Durtka <johndurtka@gmail.com>
2024-01-30 17:01:07 -0700

    added password hashing for security

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,12 +1,16 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 from flask import Flask, request, jsonify, url_for, Blueprint
+import os
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
 
 api = Blueprint('api', __name__)
+
+load_dotenv()
+
+salt = os.getenv('HASH_SALT')
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -35,7 +39,7 @@ def handle_signup():
     elif existing_email is not None:
         return jsonify({"msg": "Email already in use."}), 400
     else:
-        user = User(email = email, username = username, password = password, is_active = True)
+        user = User(email = email, username = username, password = generate_password_hash(password), is_active = True)
         db.session.add(user)
         db.session.commit()
         return jsonify({"msg": "New user created successfully.", "status": "ok"}), 200
@@ -45,13 +49,14 @@ def handle_login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
 
-    user = User.query.filter_by(username=username, password=password).first()
+    user = User.query.filter_by(username=username).first()
     if user is None:
-        return jsonify({"msg": "Incorrect username or password."}), 401
-    
-    else:
+        return jsonify({"msg": "Incorrect username."}), 401
+    if check_password_hash(user.password, password):
         access_token = create_access_token(identity=user.id)
-    return jsonify({ "token": access_token, "user_id": user.id })
+        return jsonify({ "token": access_token, "user_id": user.id })
+    else:
+        return jsonify({"msg": "Incorrect password."}), 401
 
 @api.route('/private', methods=['GET'])
 @jwt_required()
